42ff2b3ab99c7c54f8a71473465bf514a5f87400
dustendalanyi <dustenddalanyi@gmail.com>
2026-09-14 17:45:02 -0400

    Refactor portal configuration in config.js
    Updated the portal configuration to use a more secure format for the URL and anonKey.

---
+++ config.js
@@ -0,0 +1,4 @@
+window.PORTAL_CONFIG = {
+  url: "https://gskfuwyajynyovrgsdnl.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdza2Z1d3lhanlueW92cmdzZG5sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk0MTMzMTEsImV4cCI6MjEwNDk4OTMxMX0.pq5lA6h2ayO4tifpEOomtOx4j3ra6OQfw_dWZRFBAxw"
+};
